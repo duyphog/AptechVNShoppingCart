51b6936e383d488020198672bf6cea66ee37c3ba
duyphong-git <phongnddev@outlook.com>
2021-05-07 21:18:53 +0700

    update controller roles

--- Api/Controllers/AppUtilsController.cs
+++ Api/Controllers/AppUtilsController.cs
@@ -1,10 +1,13 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Helpers;
 using Api.Models;
 using Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static Api.Helpers.Utils;
 
 namespace Api.Controllers
 {
@@ -47,5 +50,30 @@ namespace Api.Controllers
 
             return result.Value.Any() ? Ok(result.Value) : BadRequest(new ErrorResponse(HttpStatusCode.BadRequest, "Warning", "Not exist value"));
         }
+
+        [HttpGet("app-roles")]
+        public async Task<ActionResult> GetAllAppRolesAsync()
+        {
+            var result = await _appUtilsService.FindAllAppRoleAsync();
+            if (!result.Succeed)
+                return BadRequest(new ErrorResponse(HttpStatusCode.BadRequest, "Fail", result.Errors));
+
+            return result.Value.Any() ? Ok(result.Value) : BadRequest(new ErrorResponse(HttpStatusCode.BadRequest, "Warning", "Not exist value"));
+        }
+
+        //[AllowAnonymous]
+        //[HttpGet("genders")]
+        //public ActionResult<IEnumerable<Gender>> GetGender()
+        //{
+        //    //var genders = new List<Gender>
+        //    //{
+        //    //    new Gender { Id = -1, Value= "UnKnown"},
+        //    //    new Gender { Id = 0, Value= "Male"},
+        //    //    new Gender { Id = 1, Value= "Female"},
+        //    //};
+
+        //    var genders = Utils.ConvertEnumToJObject()
+        //    return Ok(genders);
+        //}
     }
 }
